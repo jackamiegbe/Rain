a171757dd1ddffa35c14b59b2b990e52284abc48
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-14 00:04:27 +0100

    Done with basic ui

---
+++ Rain/WeatherVC.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherVC.swift
+//  Rain
+//
+//  Created by MACBOOK on 13/04/2017.
+//  Copyright © 2017 jack_amiegbe. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+   
+
+}
+
