1f439982aa7ade1bed6f7718a501f535a331a9bb
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-14 04:16:35 +0100

    City name showing as empty string

--- Rain/CurrentWeather.swift
+++ Rain/CurrentWeather.swift
@@ -57,10 +57,40 @@ class CurrentWeather {
         //Download Current Weather Data
         let currentWeatherURL = URL(string: CURRENT_WEATHER_URL)!
         Alamofire.request(currentWeatherURL).responseJSON { response in
+            
             let result = response.result
-            print(response)
+            
+            //we are ready from a dictioanry we aren't certain of values so us as? Dictionary<String, AnyObject>
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                
+                //if you look at the json file you see "name" is in 1st layer dictionary
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                    //the reason we are using self.value is becasue we are within closures
+                    print(self._cityName)
+                }
+                //however with weather its within a dictionary in a dictionary so as? [Dictionary<String, AnyObject>]
+                if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
+                    
+                    //6:45
+                    if let main = weather[0]["main"] as? String {
+                        self._weatherType = main.capitalized
+                        print(self._weatherType)
+                    }
+                }
+                
+                if let main = dict["main"] as? Dictionary<String, AnyObject> {
+                    
+                    if let currentTemperature = main["temp"] as? Double {
+                        let kelvinToFarenheitPreDivision = (currentTemperature * (9/5) - 459.67)
+                        let kelvinToFarenheit = Double(round(10 * kelvinToFarenheitPreDivision/10))
+                        self._currentTemp = kelvinToFarenheit
+                        print(self._currentTemp)
+                    }
+                }
+                completed()
+            }
         }
-        completed()
     }
 }
 
