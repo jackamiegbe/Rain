073b4d85449b787ad6b504f81a0ce49138489447
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-14 01:17:14 +0100

    Working

--- Rain/WeatherVC.swift
+++ Rain/WeatherVC.swift
@@ -19,16 +19,21 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //telling the tableview where all the data will come from
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    
+    //these 3 functions are the requirements/methods for a table view. in inherits from UITableView
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 6
     }
-    
+    //when dequeueReusableCell is called it looks for a cell with identifier "weatherCell" which you name cell at story board identifier of table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
         return cell
