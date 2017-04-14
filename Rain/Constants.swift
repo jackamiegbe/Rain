341c8e3134198ff0f3af95e2bc765b48cc2ffe61
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-14 02:26:10 +0100

    working
    I made a url typo

--- Rain/Constants.swift
+++ Rain/Constants.swift
@@ -17,6 +17,6 @@ let API_KEY = "dd2101764d1f724ecedb6e5c34ac3790"
 //this will tell our func when we are done downloading data 
 typealias DownloadComplete = () -> ()
 
-let CURRENT_WEATHER_URL = "(\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY))"
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
 
 //bullocks
