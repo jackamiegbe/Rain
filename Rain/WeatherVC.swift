e9c1a7363fbaa5315fe820b2f2f99dd06bdcb368
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-14 18:49:02 +0100

    Pre forecast long/lat value change

--- Rain/WeatherVC.swift
+++ Rain/WeatherVC.swift
@@ -45,13 +45,11 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func downloadForecastData(completed: @escaping DownloadComplete) {
         //Downloading forecast weather data for TableView
-        Alamofire.request(FORECAST_URL).responseJSON { response in
+        let forecastURL = URL(string: FORECAST_URL)!
+        Alamofire.request(forecastURL).responseJSON { response in
             let result = response.result
-            
             if let dict = result.value as? Dictionary<String, AnyObject> {
-                
                 if let list = dict["list"] as? [Dictionary<String, AnyObject>] {
-                    
                     for obj in list {
                         let forecast = Forecast(weatherDict: obj)
                         self.forecasts.append(forecast)
