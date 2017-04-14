d599751ae71996445ec992e4f33184dde5a75f18
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-14 01:24:00 +0100

    constants

---
+++ Rain/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Rain
+//
+//  Created by MACBOOK on 14/04/2017.
+//  Copyright © 2017 jack_amiegbe. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "dd2101764d1f724ecedb6e5c34ac3790"
